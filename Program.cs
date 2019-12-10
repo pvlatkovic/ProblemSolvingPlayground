ae7b7ac873c674b221816f0ae1a1df4051ddedc3
pvlatkovic <predrag.vlatkovic@enjoying.rs>
2019-12-10 07:19:42 +0100

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblemSolvingPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
